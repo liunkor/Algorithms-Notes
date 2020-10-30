feb429a20ccd1d884b3bfb221ab7f250711912d5
leezk <leezk007@leezks-MacBook-Pro.local>
2020-10-30 16:35:14 +0800

    add isCircularString

---
+++ src/CircularString.java
@@ -0,0 +1,49 @@
+public class CircularString {
+    private CircularString(){}
+
+    public static boolean isCircularString(String a, String b) {
+        // a = "ACTGACG";
+        // b = "TGACGAC";
+        // if a
+        if (a.equals(b)) {
+            return true;
+        }
+        if (a == null || b == null || a.length() != b.length()) {
+            return false;
+        }
+        for (int i = 0; i < a.length(); i++) {
+            if (b.startsWith(a.substring(i, a.length()))) {
+                int index = i;
+                if (a.substring(0, index).equals(b.substring(b.length()-index, b.length()))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isCircularString2(String a, String b) {
+        if (a.equals(b)) {
+            return true;
+        }
+        if (a == null || b == null || a.length() != b.length()) {
+            return false;
+        }
+        if (a.concat(a).indexOf(b) >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "AAACBTGACG";
+        String t = "TGACGAAACB";
+        var result = isCircularString2(s, t);
+        if (result) {
+            System.out.printf("%s with %s is circular string.\n", s, t);
+        }
+        else {
+            System.out.printf("%s with %s is not circular string.\n", s, t);
+        }
+    }
+}
--- src/HelloAlgorithms.java
+++ src/HelloAlgorithms.java
@@ -1,11 +1,17 @@
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class HelloAlgorithms {
     public static void main(String[] args){
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i + "  " + fib(i));
+
+        var T = 1000;
+        VisualAccumulator a = new VisualAccumulator(T, 1.0);
+        for (int t = 0; t < T; t++) {
+            a.addDataValue(StdRandom.random());
         }
+        StdOut.println(a);
+
     }
 
     public static long fib(int N) {
@@ -15,5 +21,25 @@ public class HelloAlgorithms {
         else {
             return fib(N-1) + fib(N-2);
         }
+    }
+}
+
+class VisualAccumulator {
+    private double total;
+    private int N;
+
+    public VisualAccumulator(int trials, double max) {
+        StdDraw.setXscale(0, trials);
+        StdDraw.setYscale(0, max);
+        StdDraw.setPenRadius(.005);
+    }
+
+    public void addDataValue(double val) {
+        N++;
+        total += val;
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        StdDraw.point(N, val);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.point(N, total/N);
     }
 }
